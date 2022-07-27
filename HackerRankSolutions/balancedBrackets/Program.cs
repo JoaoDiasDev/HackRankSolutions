90f5deb37dff279bc9e248c567f8639a50d36160
JoaoDias <joaodiasworking@gmail.com>
2022-07-26 21:51:34 -0300

    Finalizando diversos desafios, incuindo mergeTwoSortedLinkedLists e Pairs

---
+++ HackerRankSolutions/balancedBrackets/Program.cs
@@ -0,0 +1,69 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string isBalanced(string s)
+    {
+        var stack = new Stack<char>();
+        var dict = new Dictionary<char, char>();
+        dict.Add(')', '(');
+        dict.Add(']', '[');
+        dict.Add('}', '{');
+        var result = "YES";
+        foreach (var c in s)
+        {
+            if (dict.ContainsKey(c))
+            {
+                if (stack.Count == 0)
+                {
+                    result = "NO";
+                    break;
+                }
+                var top = stack.Pop();
+                if (top != dict[c])
+                {
+                    result = "NO";
+                    break;
+                }
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+        if (stack.Count > 0)
+        {
+            result = "NO";
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = Result.isBalanced(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ HackerRankSolutions/mergeTwoSortedLinkedLists/Program.cs
@@ -0,0 +1,156 @@
+﻿class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the mergeLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+    {
+        SinglyLinkedListNode head = null;
+        SinglyLinkedListNode tail = null;
+
+        while (head1 != null && head2 != null)
+        {
+            if (head1.data < head2.data)
+            {
+                if (head == null)
+                {
+                    head = head1;
+                }
+                else
+                {
+                    tail.next = head1;
+                }
+
+                tail = head1;
+                head1 = head1.next;
+            }
+            else
+            {
+                if (head == null)
+                {
+                    head = head2;
+                }
+                else
+                {
+                    tail.next = head2;
+                }
+
+                tail = head2;
+                head2 = head2.next;
+            }
+        }
+
+        if (head1 != null)
+        {
+            tail.next = head1;
+        }
+        else if (head2 != null)
+        {
+            tail.next = head2;
+        }
+
+        return head;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++)
+        {
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llist1Count; i++)
+            {
+                int llist1Item = Convert.ToInt32(Console.ReadLine());
+                llist1.InsertNode(llist1Item);
+            }
+
+            SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llist2Count; i++)
+            {
+                int llist2Item = Convert.ToInt32(Console.ReadLine());
+                llist2.InsertNode(llist2Item);
+            }
+
+            SinglyLinkedListNode llist3 = mergeLists(llist1.head, llist2.head);
+
+            PrintSinglyLinkedList(llist3, " ", textWriter);
+            textWriter.WriteLine();
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
